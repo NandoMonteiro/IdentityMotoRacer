6bcfdb79d64bfa00748f56c19b888e9795232094
Vinicius Fortes <vinicius_fortes@outlook.com>
2023-06-02 19:29:10 -0300

    teste

--- IdentityMotoRacer/Server/Entities/Locacao.cs
+++ IdentityMotoRacer/Server/Entities/Locacao.cs
@@ -4,53 +4,52 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace IdentityMotoRacer.Server.Entities
 {
-    public class Locacao
-    {
-        public int Id { get; set; }
-        public int ClienteId { get; set; }
-        public int VeiculoId { get; set; }
-        public DateTime DataRetirada { get; set; }
-        public DateTime HoraRetirada { get; set; }
-        public DateTime DataDevolucao { get; set; }
-        public DateTime Horadevolucao { get; set; }
+	public class Locacao
+	{
+		public int Id { get; set; }
+		public int ClienteId { get; set; }
+		public int VeiculoId { get; set; }
+		public DateTime DataRetirada { get; set; }
+		public DateTime HoraRetirada { get; set; }
+		public DateTime DataDevolucao { get; set; }
+		public DateTime Horadevolucao { get; set; }
 
-        [Column(TypeName = "decimal(10,2)")]
-        public decimal ValorLocacao { get; set; }
+		[Column(TypeName = "decimal(10,2)")]
+		public decimal ValorLocacao { get; set; }
 
-        [Column(TypeName = "decimal(10,2)")]
-        public decimal ValorDesconto { get; set; }
-        public int QuilometragemInicial { get; set; }
-        public int QuilometragemFinal { get; set; }
+		[Column(TypeName = "decimal(10,2)")]
+		public decimal ValorDesconto { get; set; }
+		public int QuilometragemInicial { get; set; }
+		public int QuilometragemFinal { get; set; }
 
-        [MaxLength(300)]
-        public string Observacao { get; set; } = string.Empty;
-        public enum StatusLocacao
-        {
-            Pendente,
-            Emandamento,
-            Concluida,
-            Cancelada
-        }
+		[MaxLength(300)]
+		public string Observacao { get; set; } = string.Empty;
+		public enum StatusLocacao
+		{
+			Pendente,
+			Emandamento,
+			Concluida,
+			Cancelada
+		}
 
-        public Veiculo? Veiculos { get; set; }
-        public Cliente? Clientes { get; set; }
-        public LocacaoItemServico? LocacaoItemServicos { get; set; }
+		public Veiculo? Veiculos { get; set; }
+		public Cliente? Clientes { get; set; }
+		public LocacaoItemServico? LocacaoItemServicos { get; set; }
 
-        [Column(TypeName = "decimal(10,2)")]
-        public decimal ValorServico { get; set; }
+		[Column(TypeName = "decimal(10,2)")]
+		public decimal ValorServico { get; set; }
 
-        public ItemServico? Items { get; set; }
+		public ItemServico? Items { get; set; }
 
-        [Column(TypeName = "decimal(10,2)")]
-        public decimal ValorItem { get; set; }
+		[Column(TypeName = "decimal(10,2)")]
+		public decimal ValorItem { get; set; }
 
-        [Column(TypeName = "decimal(10,2)")]
-        public decimal ValorItemTotal { get; set; }
+		[Column(TypeName = "decimal(10,2)")]
+		public decimal ValorItemTotal { get; set; }
 
-        [Column(TypeName = "decimal(10,2)")]
-        public decimal Valortotallocacao { get; set; }
+		[Column(TypeName = "decimal(10,2)")]
+		public decimal Valortotallocacao { get; set; }
 
-        public ICollection<LocacaoItemServico> ItemServicos { get; set; }
-            = new List<LocacaoItemServico>();
-    }
+		public ICollection<LocacaoItemServico> ItemServicos { get; set; } = new List<LocacaoItemServico>();//teste
+	}
 }
